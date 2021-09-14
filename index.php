e134a5b65ac73603c69f2748882305f9b20b9b2b
watcharaphong <87680218+watcharaphong@users.noreply.github.com>
2021-09-14 13:23:34 +0700

    first commit

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php echo '<p>Hello World--My name is วัชรพงษ์ ภูมิรัง รหัสนิสิต 63010974017 </p>'; ?>
  </body>
 </html>
